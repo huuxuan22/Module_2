b2d3424dcf8ef02aaa9feff3fefe4ff654e783e7
TranXuan2212 <tranduonghuuxuan22@gmail.com>
2023-11-11 15:35:25 +0700

    upbaitap

--- phan_5/phan_5/src/AccessModifier/Circle.java
+++ phan_5/phan_5/src/AccessModifier/Circle.java
@@ -1,5 +1,7 @@
 package AccessModifier;
 
+import java.util.Scanner;
+
 public class Circle {
     protected double radius;
     protected String color;
@@ -26,5 +28,12 @@ public class Circle {
     public double getArea(){
         double area = 3.14 * Math.pow(radius,2);
         return area;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Circle circle = new Circle();
+        circle.radius = sc.nextDouble();
+        System.out.println("ban kinh cua hinh tron la: "+ circle.getRadius());
+        System.out.println("dien tich cua hinh tron la: "+ circle.getArea());
     }
 }
--- phan_5/phan_5/src/ChiGhi/Student.java
+++ phan_5/phan_5/src/ChiGhi/Student.java
@@ -19,4 +19,11 @@ public class Student {
     public String getClasses(){
         return classes;
     }
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("tran duong huu xuan");
+        student.setClasses("V01");
+        System.out.println("Ho va ten: "+ student.getName());
+        System.out.println("Lop: "+ student.getClasses());
+    }
 }
